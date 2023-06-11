fb9b90766be74c438e8ddea495d8af418d742e93
kavinsky-spy <juliorapa@hotmail.com>
2023-06-11 13:22:17 -0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            child: Text('FUUUUCK'),
+          ),
+        ),
+      ),
+    );
+  }
+}
