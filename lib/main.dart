f8764e55430e152441b6516f438b31a172e3bb78
kavinsky-spy <juliorapa@hotmail.com>
2023-06-14 00:27:58 -0300

    notes to strech the page

--- lib/main.dart
+++ lib/main.dart
@@ -16,74 +16,78 @@ class XylophoneApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Container(
             child: Center(
                 child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                TextButton(
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.red),
                     onPressed: () {
                       playSound(1);
                     },
-                    child: Container(
-                      width: 150,
-                      height: 50,
-                      color: Colors.red,
-                    )),
-                TextButton(
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.deepOrange),
                     onPressed: () {
                       playSound(2);
                     },
-                    child: Container(
-                      width: 150,
-                      height: 50,
-                      color: Colors.deepOrange,
-                    )),
-                TextButton(
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.yellow),
                     onPressed: () {
                       playSound(3);
                     },
-                    child: Container(
-                      width: 150,
-                      height: 50,
-                      color: Colors.amberAccent,
-                    )),
-                TextButton(
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.green),
                     onPressed: () {
                       playSound(4);
                     },
-                    child: Container(
-                      width: 150,
-                      height: 50,
-                      color: Colors.green,
-                    )),
-                TextButton(
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.green[900]),
                     onPressed: () {
                       playSound(5);
                     },
-                    child: Container(
-                      width: 150,
-                      height: 50,
-                      color: Colors.green[900],
-                    )),
-                TextButton(
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
                     onPressed: () {
                       playSound(6);
                     },
-                    child: Container(
-                      width: 150,
-                      height: 50,
-                      color: Colors.blue,
-                    )),
-                TextButton(
+                    child: Text(''),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.purple),
                     onPressed: () {
                       playSound(7);
                     },
-                    child: Container(
-                      width: 150,
-                      height: 50,
-                      color: Colors.purple,
-                    )),
+                    child: Text(''),
+                  ),
+                ),
               ],
             )),
           ),
